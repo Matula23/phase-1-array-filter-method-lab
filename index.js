c12c31698cf83584832d3488f26fd418f3aa3169
Maciej Matula <maciek.matula@gmail.com>
2022-06-01 22:17:29 -0500

    commit tree 84bae8623fdd137cebb6be400392aa4629d065cf

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, drivers){
+    const newArray = array.filter(element => element.toUpperCase() === drivers.toUpperCase())
+    return newArray
+}
+
+function fuzzyMatch(array, letters){
+    const newArray = array.filter(element => element.indexOf(letters) === 0)
+    return newArray
+}
+
+function matchName(array, propertyName){
+    const newArray = array.filter(element => element.name === propertyName)
+    return newArray
+}
